6012b7fc38878ede1289ac8f20afccde91f86d8e
unknown <bh.azulay@gmail.com>
2019-02-10 23:32:41 +0200

    add flag if smal image exists

--- src/image.py
+++ src/image.py
@@ -3,7 +3,8 @@ import os
 import cv2
 import numpy as np
 
-img = cv2.imread('..\\images\\all_and_lee.PNG')
+# img = cv2.imread('..\\images\\all_and_lee.PNG')
+img = cv2.imread('C:\\Users\\Boaz Azulay\\Desktop\\test.JPEG')
 img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
 template = cv2.imread('..\\images\\lee.PNG',0)
@@ -13,9 +14,13 @@ res = cv2.matchTemplate(img_gray, template, cv2.TM_CCOEFF_NORMED)
 threshold = 0.8
 loc = np.where(res >= threshold)
 
+flag = False
 for pt in zip(*loc[::-1]):
+    if pt is not None:
+        flag = True
     cv2.rectangle(img, pt, (pt[0]+w, pt[1]+h), (0, 255, 255), 2)
 
+print(flag)
 cv2.imshow('detected', img)
 
 # leesin_img = cv2.imread('C:\\Users\\Boaz Azulay\\Desktop\\lee.PNG',0)

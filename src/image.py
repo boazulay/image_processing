da9e6d6f2958ae869b00181ef65ad5311fe56b45
unknown <bh.azulay@gmail.com>
2019-02-10 23:57:09 +0200

    add function if jungler arrive

--- src/image.py
+++ src/image.py
@@ -1,29 +1,45 @@
 import os
+import time
 
 import cv2
 import numpy as np
+from PIL import ImageGrab
 
-img = cv2.imread('..\\images\\all_and_lee.PNG')
-# img = cv2.imread('..\\images\\no_match.JPEG')
-img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+def jungler_arrive():
+    print("jungler_arrive not implemted yets")
 
-template = cv2.imread('..\\images\\lee.PNG',0)
-w, h = template.shape[::-1]
+def jungler_missing():
+    print("jungler_missing not implemted yets")
 
-res = cv2.matchTemplate(img_gray, template, cv2.TM_CCOEFF_NORMED)
-threshold = 0.8
-loc = np.where(res >= threshold)
+time_to_sleep=1
 
-flag = False
-for pt in zip(*loc[::-1]):
-    if pt is not None:
-        flag = True
-    cv2.rectangle(img, pt, (pt[0]+w, pt[1]+h), (0, 255, 255), 2)
+while True:
+    time.sleep(time_to_sleep)
+    screenshot = ImageGrab.grab()
+    screenshot.save("new.png")
+    img = cv2.imread("new.png")
+    # img = cv2.imread('..\\images\\all_and_lee.PNG')
+    # img = cv2.imread('..\\images\\no_match.JPEG')
+    img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
-print(flag)
-cv2.imshow('detected', img)
+    template = cv2.imread('..\\images\\lee.PNG',0)
+    w, h = template.shape[::-1]
 
-# leesin_img = cv2.imread('C:\\Users\\Boaz Azulay\\Desktop\\lee.PNG',0)
-# cv2.imshow('image', leesin_img)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
+    res = cv2.matchTemplate(img_gray, template, cv2.TM_CCOEFF_NORMED)
+    threshold = 0.8
+    loc = np.where(res >= threshold)
+
+    flag = False
+    for pt in zip(*loc[::-1]):
+        if pt is not None:
+            flag = True
+        # cv2.rectangle(img, pt, (pt[0]+w, pt[1]+h), (0, 255, 255), 2)
+
+    if flag == True:
+        jungler_arrive()
+    else:
+        jungler_missing()
+    
+    # cv2.imshow('detected', img)
+    # cv2.waitKey(0)
+    # cv2.destroyAllWindows()
